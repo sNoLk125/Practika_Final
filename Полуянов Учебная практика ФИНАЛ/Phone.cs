20af498051a294587b986df2aa2fdd77dde3030b
sNoLk125 <oluyan178@mail.ru>
2022-04-30 14:26:43 +0500

    Добавьте файлы проекта.

---
+++ Полуянов Учебная практика ФИНАЛ/Phone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Полуянов_Учебная_практика_ФИНАЛ
+{
+    public class Phone
+    {
+        public string No { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string DateOfBirht { get; set; }
+        public string College { get; set; }
+        public int Expirience { get; set; }
+        public string Salary { get; set; }
+    }
+}
